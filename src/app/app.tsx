c2dfaefeaca0bccc37873ded17b4e8bca7ac7896
Viktor Timofeev <l_893@mail.ru>
2026-04-07 18:55:05 +0400

    chore: remove unused app styles and note factory

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,16 +1,12 @@
 import { AppRouter } from './router';
 import { AuthProvider } from '@features/auth';
 
-import styles from './app.module.css';
-
 export const App = (): React.JSX.Element => {
   return (
     <>
-      <div className={styles.app}>
-        <AuthProvider>
-          <AppRouter />
-        </AuthProvider>
-      </div>
+      <AuthProvider>
+        <AppRouter />
+      </AuthProvider>
     </>
   );
 };
